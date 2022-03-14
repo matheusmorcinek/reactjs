583fbc95a6125a8552faddc2c2e6755b7e78e8da
Matheus Morcinek <matheusmorcinek@gmail.com>
2022-03-14 14:04:24 -0300

    base project for react js debounce example

---
+++ reactjs-debounce-example/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Debounce – How to Delay a Function in React JS using Lodash
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
