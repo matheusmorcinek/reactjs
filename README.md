a9c42865cc23e57f459d9a15aae82276badc5698
Matheus Morcinek <matheusmorcinek@gmail.com>
2022-03-24 21:48:19 -0300

    pure js debounce example

---
+++ README.md
@@ -0,0 +1,3 @@
+reactjs-lodash-debounce-example
+
+https://www.freecodecamp.org/news/debounce-and-throttle-in-react-with-hooks/
---
+++ reactjs-vanilla-debounce-example/src/components/autocomplete/autocomplete.jsx
@@ -0,0 +1,92 @@
+import React, { useState, useCallback, useEffect } from "react";
+import lodash from 'lodash';
+import axios from 'axios';
+import classnames from 'classnames';
+
+const ITEMS_API_URL = 'http://localhost:3001/api/countries';
+const DEBOUNCE_DELAY = 500;
+
+const fetchItems = async (value, setItems, setLoading) => {
+    setItems([]);
+    setLoading(true);
+
+    try {
+
+        const { data } = await axios(`${ITEMS_API_URL}?query=${value}`);
+        setItems(data);
+    } catch (error) {
+
+        console.error(error.message);
+    }
+
+    setLoading(false);
+};
+
+const Autocomplete = ({ onSelectItem }) => {
+
+    const [loading, setLoading] = useState(false);
+    const [inputText, setInputText] = useState('');
+    const [items, setItems] = useState([]);
+    const [timeoutId, setTimeoutId] = useState('');
+
+    // const delayedItemsFetch = useCallback(lodash.debounce(inputText => fetchItems(inputText, setItems, setLoading), DEBOUNCE_DELAY), []);
+
+    useEffect(() => {
+        console.log('Autocomplete com ponent did render')
+        console.log('timeoutId ', timeoutId)
+    });
+
+    const delayedItemsFetch = (inputText) => {
+        if (timeoutId) {
+            console.log('clearTimeout ', timeoutId)
+            clearTimeout(timeoutId);
+        }
+        const timeOutId = setTimeout(() => {
+            console.log('delayedItemsFetch ', inputText);
+
+            fetchItems(inputText, setItems, setLoading);
+
+        }, DEBOUNCE_DELAY);
+        setTimeoutId(timeOutId);
+    }
+
+    const handleInputText = (event) => {
+        setInputText(event.target.value);
+        delayedItemsFetch(event.target.value);
+    }
+
+    const wrapperControlClassNames = classnames('control', { 'is-loading': loading });
+
+    return (
+        <div className="wrapper">
+            <div className={wrapperControlClassNames}>
+                <input
+                    type="text"
+                    className="input"
+                    value={inputText}
+                    onChange={handleInputText}
+                />
+            </div>
+            {
+                items.length > 0 && (
+                    <div className="list is-hoverable" >
+                        {
+                            items.map(item => (
+                                <a
+                                    key={item}
+                                    className="list-item"
+                                    onClick={() => onSelectItem(item)}
+                                >
+                                    {item}
+                                </a>)
+                            )
+                        }
+                    </div>
+                )
+            }
+
+        </div>
+    );
+};
+
+export default Autocomplete;
