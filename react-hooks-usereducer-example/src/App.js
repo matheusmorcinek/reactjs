ee6ddb783db5fae43d1458bbe6957dc8a8425e03
Matheus Morcinek <matheusmorcinek@gmail.com>
2022-07-15 12:53:33 -0300

    starting with useState

--- react-hooks-usereducer-example/src/App.js
+++ react-hooks-usereducer-example/src/App.js
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount(prevCount => prevCount - 1);
+  };
+
+  useEffect(() => {
+    console.log('App did mount')
+  }, []);
+
+  useEffect(() => {
+    console.log('App did render')
+  });
+
   return (
-    <div className="App">
-      <h1>hello matheus</h1>
-    </div>
+    <>
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
+    </>
   );
 }
 
